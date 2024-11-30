db13af7aae472d278f82cef45c4a53442dae1560
horvvivien <horvvivien23@gmail.com>
2024-11-30 12:48:30 +0100

    Final animation

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator animator; // Az animációért felelõs Animator
+    public Canvas canvas; // A megjelenítendõ Canvas
+    public GameObject animationObject; // Az animáció GameObject-je
+
+    private void Start()
+    {
+        // Kezdetben a Canvas láthatatlan
+        if (canvas != null)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+
+        // Indítsd el az animációt
+        if (animator != null)
+        {
+            animator.Play("AnimationName"); // Cseréld ki az animáció nevére
+        }
+
+        // 3 másodperc után hajtsd végre a Canvas megjelenítését és az animáció elrejtését
+        Invoke("EndAnimationAndShowCanvas", 3f);
+    }
+
+    private void EndAnimationAndShowCanvas()
+    {
+        // Az animáció GameObject deaktiválása
+        if (animationObject != null)
+        {
+            animationObject.SetActive(false);
+        }
+
+        // A Canvas megjelenítése
+        if (canvas != null)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+}
